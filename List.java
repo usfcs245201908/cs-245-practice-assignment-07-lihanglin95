ee56688bf568dc36d78bf1d939926ba6368212c4
lihanglin95 <37127371+lihanglin95@users.noreply.github.com>
2019-10-16 22:12:11 -0700

    Add throws Exception

--- List.java
+++ List.java
@@ -2,7 +2,7 @@
 public interface List<T> {
 	
 	public void add(T item);
-	public void add(int pos, T item);
+	public void add(int pos, T item) throws Exception;
 	public T get(int pos);
 	public T remove(int pos);
 	public int size();
